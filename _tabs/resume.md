c69c1deafb59e754730af596a3db217d5d4a36ce
Michael O’Brien <mobrien@cheekyghost.com>
2026-08-06 09:01:47 +1000

    [DEPOLY] Deploying site

--- _tabs/resume.md
+++
@@ -1,7 +0,0 @@
----
-layout: archives
-icon: fas fa-scroll
-order: 5
-title: Resume
-external_url: https://mobrien.craft.me/resume-2026
----
---
+++ assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1785970840',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/about/',
+      
+        '/mimic/',
+      
+        '/contact/',
+      
+        '/resume/',
+      
+        '/open-source/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
