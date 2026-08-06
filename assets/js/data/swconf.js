e40459a81b8abce64e55149f5fe444ead7e8ae33
Michael O’Brien <mobrien@cheekyghost.com>
2026-08-06 11:26:00 +1000

    [DEPOLY] Deploying with custom domain settings

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1785977774',resources: [
+    cacheName: 'chirpy-1785979520',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
