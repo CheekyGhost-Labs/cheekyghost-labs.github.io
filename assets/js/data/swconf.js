8372765b8727fa0a0aba2ed07d93af07613488f8
Michael O’Brien <mobrien@cheekyghost.com>
2026-08-06 10:58:44 +1000

    [DEPOLY] Deploying with updated url and CNAME

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1785970840',resources: [
+    cacheName: 'chirpy-1785977774',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
