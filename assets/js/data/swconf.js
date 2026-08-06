3a76df70ea09b4a71c12722c38ae8c85e1df376d
Michael O’Brien <mobrien@cheekyghost.com>
2026-08-06 11:52:16 +1000

    [DEPOLY] Deploying with correct store url

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1785979520',resources: [
+    cacheName: 'chirpy-1785981090',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
